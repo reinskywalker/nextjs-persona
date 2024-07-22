16962663e85ea01f5d3f880e3609732e939ef445
reinskywalker <andre.reynaldi@ai.astra.co.id>
2024-07-22 17:53:18 +0700

    next config

---
+++ out/_next/static/YEEEUsSgvKi8wE3Q1UYrT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-bcf0ba6d4f231caf.js","static/chunks/d7eeaac4-822b3b34549db9b0.js","static/chunks/pages/index-c30d35736b9723f7.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
