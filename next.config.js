141c956797ab0d6d7399c0ce5ff9e840deb69c6e
reinskywalker <andre.reynaldi@ai.astra.co.id>
2024-09-06 12:18:44 +0700

    base path to index

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/nextjs-persona',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+    reactStrictMode: true,
+    basePath: '',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
